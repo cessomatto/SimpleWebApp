d0be5aedfc0865b0dbcfb770343885dd63ff31b2
cessomatto <scibiz@gmail.com>
2016-07-11 16:12:42 +0100

    This is my first commit! Super!

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -3,10 +3,10 @@ package com.develogical.app;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
+        if (query.toLowerCase().contains("alessandro")) {
+            return "Alessandro is an " +
+                    "Italian poet, playwright, and actor, widely regarded as the greatest " +
+                    "writer in the Italian language and the world's pre-eminent dramatist.";
         }
         return "";
     }
