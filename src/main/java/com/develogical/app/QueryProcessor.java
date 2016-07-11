dabb6b224a79a80a56540d7a8152373af2e96f6a
cessomatto <scibiz@gmail.com>
2016-07-11 16:20:50 +0100

    This is my second commit - changed back to Shakespeare

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -3,7 +3,7 @@ package com.develogical.app;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("alessandro")) {
+        if (query.toLowerCase().contains("shakespeare")) {
             return "Alessandro is an " +
                     "Italian poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the Italian language and the world's pre-eminent dramatist.";
